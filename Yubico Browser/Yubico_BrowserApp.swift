e5fea022fc83979a186e064f43fd3d2fb22f0687
Jens Utbult <jens.utbult@yubico.com>
2021-12-09 16:24:47 +0100

    Initial Commit

---
+++ Yubico Browser/Yubico_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yubico_BrowserApp.swift
+//  Yubico Browser
+//
+//  Created by Jens Utbult on 2021-12-09.
+//
+
+import SwiftUI
+
+@main
+struct Yubico_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
